480ecee8a89cd66fc617012b89a140c146ca1303
juanjo23 <juanjo23gallardo@gmail.com>
2021-02-27 14:49:39 -0600

    v1

--- success.php
+++ success.php
@@ -5,6 +5,7 @@ require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
+
 $collection_id = $_GET['collection_id'];
 $collection_status = $_GET['collection_status'];
 $external_reference = $_GET['external_reference'];
